3268dd827aca260bf0957e8ab80cfd09ff8fde56
Ismael Gbian <ismael.gbian@gmail.com>
2021-05-26 23:25:55 -0400

    feat: Added the hover

--- TP3/src/scripts/hover.js
+++ TP3/src/scripts/hover.js
@@ -13,7 +13,25 @@
  * @param {Function} unselectTicks The function to call to remove "selected" mode from the ticks
  */
 export function setRectHandler (xScale, yScale, rectSelected, rectUnselected, selectTicks, unselectTicks) {
-  // TODO : Select the squares and set their event handlers
+  // DONE : Select the squares and set their event handlers
+  d3.select('#graph-g')
+    .selectAll('.heatmap-rect')
+    .each((data, rectIndex, rectangles) => {
+      // Get the group corresponding to the rectangle
+      const rect = rectangles[rectIndex]
+      const group = d3.select(rect).node().parentNode
+
+      // Add listeners to the group (not the rectangle because of bubbling effects when hovering the text)
+      d3.select(group)
+        .on('mouseenter', () => {
+          rectSelected(d3.select(rect), xScale, yScale)
+          selectTicks(data.Arrond_Nom, data.Plantation_Year)
+        })
+        .on('mouseleave', () => {
+          rectUnselected(d3.select(rect), xScale, yScale)
+          unselectTicks()
+        })
+    })
 }
 
 /**
@@ -28,9 +46,33 @@ export function setRectHandler (xScale, yScale, rectSelected, rectUnselected, se
  * @param {*} yScale The yScale to be used when placing the text in the square
  */
 export function rectSelected (element, xScale, yScale) {
-  // TODO : Display the number of trees on the selected element
+  // DONE : Display the number of trees on the selected element
   // Make sure the nimber is centered. If there are 1000 or more
   // trees, display the text in white so it contrasts with the background.
+
+  const data = element.data()[0]
+  const numberOfTrees = data.Counts
+  const group = d3.select(element.node().parentNode)
+
+  const elementBox = element.node().getBBox()
+  const transaltionX = elementBox.width * 0.5
+  const translationY = elementBox.height * 0.5
+
+  const textColor = numberOfTrees >= 1000 ? 'white' : 'black'
+
+  group
+    .append('text')
+    .attr('text-anchor', 'middle')
+    .attr('dominant-baseline', 'middle')
+    .style('fill', textColor)
+    .text(numberOfTrees)
+    .attr('x', () => xScale(data.Plantation_Year))
+    .attr('y', () => yScale(data.Arrond_Nom))
+    .attr('transform', 'translate(' + transaltionX + ', ' + translationY + ')')
+
+  element.select('rect')
+    .style('opacity', 0.75)
+    .style('font-weight', 'bold')
 }
 
 /**
@@ -43,7 +85,14 @@ export function rectSelected (element, xScale, yScale) {
  * @param {*} element The selection of rectangles in "selected" state
  */
 export function rectUnselected (element) {
-  // TODO : Unselect the element
+  // DONE : Unselect the element
+  const group = d3.select(element.node().parentNode)
+
+  group.selectAll('text').remove()
+
+  element
+    .style('opacity', 1)
+    .style('font-weight', 'normal')
 }
 
 /**
@@ -53,12 +102,28 @@ export function rectUnselected (element) {
  * @param {number} year The year associated with the tick text to make bold
  */
 export function selectTicks (name, year) {
-  // TODO : Make the ticks bold
+  // DONE : Make the ticks bold
+  d3.selectAll('.x')
+    .selectAll('text')
+    .filter((text) => text === year)
+    .style('font-weight', 'bold')
+
+  d3.selectAll('.y')
+    .selectAll('text')
+    .filter((text) => text === name)
+    .style('font-weight', 'bold')
 }
 
 /**
  * Returns the font weight of all ticks to normal.
  */
 export function unselectTicks () {
-  // TODO : Unselect the ticks
+  // DONE : Unselect the ticks
+  d3.select('.x')
+    .selectAll('text')
+    .style('font-weight', 'normal')
+
+  d3.select('.y')
+    .selectAll('text')
+    .style('font-weight', 'normal')
 }
