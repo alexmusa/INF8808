191e789da37d0830a7385ed5c695c3cb4f34d6e6
Ismael Gbian <ismael.gbian@gmail.com>
2021-06-20 04:46:49 -0400

    feat: Added viz for viz2

--- Project/src/scripts/viz2/viz.js
+++ Project/src/scripts/viz2/viz.js
@@ -1,8 +1,95 @@
+
 /**
- * Legacy functions:
- * positionLabels (g, width, height)
- * drawCircles (data, rScale, colorScale)
- * setCircleHoverHandler (tip)
- * moveCircles (xScale, yScale, transitionDuration)
- * setTitleText (year)
+ * @param {object} category The category being processed
  */
+export function update (category) {
+  const data = parseData(category)
+  const rows = data.map(r => Object.values(r))
+
+  showTitle(category)
+  clearTable()
+
+  if (rows.length) drawTable(Object.keys(data[0]), rows)
+}
+
+/**
+ * @param {object} category The category being processed
+ */
+function showTitle (category) {
+  d3.select('.table-label').remove()
+
+  const title = d3.select('.graph-2')
+    .insert('div', ':first-child')
+    .attr('class', 'table-label')
+
+  title.append('label')
+    .text(category.data.length ? 'Showing contracts with:' : 'No contracts for:')
+    .style('font-weight', 'bold')
+
+  const attributes = title.append('p')
+  let isFirstAttr = true
+  category.attributes.forEach(attr => {
+    const key = Object.keys(attr)[0]
+    const seperator = isFirstAttr ? '' : ', and'
+    attributes.append('text')
+      .text(`${seperator} `)
+    attributes.append('text')
+      .text(`${key}: `)
+      .style('font-weight', 'bold')
+    attributes.append('text')
+      .text(attr[key])
+    isFirstAttr = false
+  })
+}
+
+/**
+ *  Removes the table
+ */
+function clearTable () {
+  d3.select('#table').select('table').remove()
+}
+
+/**
+ * @param {string[]} attributes All attributes to display in the table header
+ * @param {string[][]} rows All rows corresponding to the attributes
+ */
+function drawTable (attributes, rows) {
+  // table
+  const table = d3.select('#table')
+    .append('table')
+
+  // headers
+  table.append('thead').append('tr')
+    .selectAll('th')
+    .data(attributes)
+    .enter().append('th')
+    .text(d => d)
+
+  // data
+  table.append('tbody')
+    .selectAll('tr').data(rows)
+    .enter().append('tr')
+    .selectAll('td')
+    .data(d => d)
+    .enter().append('td')
+    .text(d => d)
+}
+
+/**
+ * Removes the attributes that are in category.attributes from category.data and parses the dates
+ *
+ * @param {object} category The category to parse
+ * @returns {object} The same category without the excluded attributes in its data
+ */
+function parseData (category) {
+  return category.data.map(d => {
+    const newContract = {}
+    Object.keys(d).forEach(k => {
+      if (category.attributes.every(attr => k !== Object.keys(attr)[0])) {
+        newContract[k] = d[k]
+      }
+    })
+    newContract.Date = d3.timeFormat('%Y %b %d')(newContract.Date)
+    return newContract
+  })
+}
