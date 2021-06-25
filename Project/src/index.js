6e8bde40f4bc291514c23c217e6beccfd8b1db86
Ismael Gbian <ismael.gbian@gmail.com>
2021-06-25 02:43:20 -0400

    feat: Integrated the line chart

--- Project/src/index.js
+++ Project/src/index.js
@@ -4,6 +4,7 @@ import Viz1 from './scripts/viz1/main'
 import Viz2 from './scripts/viz2/main'
 import * as checkboxes from './scripts/checkboxes'
 import * as preprocess from './scripts/preprocess'
+import Viz3 from './scripts/viz3/main'
 
 (function (d3) {
   d3.csv('./telefilmCanada.csv').then((data) => {
@@ -11,7 +12,7 @@ import * as preprocess from './scripts/preprocess'
     const checkBoxesHandler = new checkboxes.CheckBoxesHandler()
 
     const viz2 = new Viz2(dataHandler, checkBoxesHandler)
-    const viz1 = new Viz1(dataHandler, checkBoxesHandler, viz2)
+    const viz1 = new Viz1(dataHandler, checkBoxesHandler, viz2, new Viz3())
 
     checkBoxesHandler.setCheckboxes(dataHandler.attributes, () => viz1.update())
   })
