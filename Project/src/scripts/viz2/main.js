f0015cb3656c4517061fe3a107331fa65bb352be
Ismael Gbian <ismael.gbian@gmail.com>
2021-06-25 02:43:55 -0400

    feat: Added the double click handler for the table

--- Project/src/scripts/viz2/main.js
+++ Project/src/scripts/viz2/main.js
@@ -4,10 +4,32 @@ export default class Viz2 {
   constructor (dataHandler, checkBoxesHandler) {
     this.dataHandler = dataHandler
     this.checkBoxesHandler = checkBoxesHandler
+    this.doubleClickHandler = new DoubleClickHandler()
   }
 
   // This method is called whenever the user changes their selection
   update (category) {
-    viz.update(category)
+    if (this.doubleClickHandler.isDoubleClick()) {
+      viz.update(category)
+      d3.select('#graph-2-modal-toggle').dispatch('click')
+    }
+  }
+}
+
+class DoubleClickHandler {
+  constructor () {
+    this.waitingSecondClick = false
+  }
+
+  isDoubleClick () {
+    if (this.waitingSecondClick) {
+      return true
+    } else {
+      this.waitingSecondClick = true
+      setTimeout(() => {
+        this.waitingSecondClick = false
+      }, 500)
+      return false
+    }
   }
 }
