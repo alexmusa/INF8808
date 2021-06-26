91ba630f89345375ec6bd300f4bd54cf68a3121d
Gabriel St-Denis <gabriel.st-denis@polymtl.ca>
2021-06-26 18:47:14 -0400

    Correction to previous push

--- Project/src/index.js
+++ Project/src/index.js
@@ -18,14 +18,11 @@ import Viz3 from './scripts/viz3/main'
     checkBoxesHandler.setCheckboxes(dataHandler.attributes, () => viz1.update())
 
     /* Display hidden elements to display all elements at the same moment */
-    var title = d3.select('header > h2 > text')
-    title.style('visibility', 'visible')
-
     d3.select('#history-btn').style('visibility', 'visible')
     d3.select('#checkbox-cont-label').style('visibility', 'visible')
 
     var wt = walkthrough.init()
-    title.on('click', () => wt.start())
+    d3.select('header > h2 > text').on('click', () => wt.start())
     wt.start()
   })
 })(d3)
