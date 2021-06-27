ee4f42bb72e3084e3398519fe8392a92c5d443f0
Ismael Gbian <ismael.gbian@gmail.com>
2021-06-27 17:50:04 -0400

    chore: Added authors names

--- Project/src/index.js
+++ Project/src/index.js
@@ -7,6 +7,11 @@ import * as preprocess from './scripts/preprocess'
 import * as walkthrough from './scripts/walkthrough'
 import Viz3 from './scripts/viz3/main'
 
+/**
+ * @file This file is the entry-point for the the code for the final project for the course INF8808.
+ * @author Alexandre Musaraganyi, Gabriel St-Denis, Ismael Gbian
+ */
+
 (function (d3) {
   d3.csv('./telefilmCanada.csv').then((data) => {
     const dataHandler = new preprocess.DataHandler(data)
